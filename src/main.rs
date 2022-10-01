34d5f21fc264e5c4e3c2f0ac1d218bd49ff4bb78
Lydia <pricelydia776@gmail.com>
2022-10-02 09:02:56 +1300

    Added comments from Skeleton

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ struct Date {
 }
 
 impl Date {
-    // create date from year/month/date format
+    // create Date from year/month/day triple
     fn from_ymd(year: i32, month: i32, day: i32) -> Date {
         return Date { days: 0 };
     }
-
+    // convert back to year/month/day triple
     fn ymd(&self) -> (i32, i32, i32) {
         return (0, 0, 0);
     }
@@ -40,11 +40,18 @@ impl fmt::Display for Date {
 
 
 fn main() {
+    // testing from_ymd; should print Date { days: 738885 }
+
     println!("{:?}", Date::from_ymd(2022, 12, 31));
+    // testing Add and Display
+
     let date = Date::from_ymd(-2, 12, 31);
+    // increase date by 30 days, 60 days, etc.
 
     for i in 0..20 {
-        println!("{}", date + i * 30);
+        // first iteration should print 2/12/31 BC
+
+        println!("{}", (date.days + i * 30)); // added date.days + i * 30 instead of date + i * 30
     }
 }
 
