2c9b8441ed9aa7046fd7ed67d42795a25e6bb0b7
Lydia <pricelydia776@gmail.com>
2022-10-02 17:46:04 +1300

    Minor Update

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,13 @@ struct Date {
 impl Date {
     // create Date from year/month/day triple
     fn from_ymd(year: i32, month: i32, day: i32) -> Date {
-        let x: i32 = if is_leapyear(year) { 29 } else { 28 };
-        let month_days: [i32; 12] = [31, x, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let month_days: [i32; 12] = [31, if is_leapyear(year) { 29 } else { 28 },
+            31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         let mut days_count: i32 = 0;
 
         if year >= 0 {
             for i in 0..(year) {
-                if is_leapyear(i) {
-                    days_count += 366;
-                } else {
-                    days_count += 365
-                }
+                days_count += find_year_days(i);
             }
             for i in 0..(month - 1) {
                 days_count += month_days[i as usize];
@@ -28,11 +24,7 @@ impl Date {
             days_count += day - 1;
         } else {
             for i in year + 1..0 {
-                if is_leapyear(i) {
-                    days_count += 366;
-                } else {
-                    days_count += 365
-                }
+                days_count += find_year_days(i);
             }
 
             for i in month..12 {
@@ -47,53 +39,45 @@ impl Date {
     }
     // convert back to year/month/day triple
     fn ymd(&self) -> (i32, i32, i32) {
-        let mut days: i32 = self.days;
+        let mut days_count: i32 = self.days;
         let mut year: i32 = 0;
-        let mut month: i32 = 0;
+        let mut month: i32;
         let mut day: i32 = 1;
 
-        if days >= 0 {
+        if days_count >= 0 {
             month = 1;
-            while days >= 365 {
-                if is_leapyear(year) {
-                    days -= 366;
-                } else {
-                    days -= 365;
-                }
+            while days_count >= 365 {
+                days_count -= find_year_days(year);
                 year += 1;
             }
 
-            let x: i32 = if is_leapyear(year) { 29 } else { 28 };
-            let month_days: [i32; 12] = [31, x, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+            let month_days: [i32; 12] = [31, if is_leapyear(year) { 29 } else { 28 },
+                31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
             let mut i: i32 = 0;
-            while days >= month_days[i as usize] {
-                days -= month_days[i as usize];
+
+            while days_count >= month_days[i as usize] {
+                days_count -= month_days[i as usize];
                 month += 1;
                 i += 1;
             }
-            day += days;
+            day += days_count;
         } else {
             year = -1;
-            month = 12;
-            days *= -1;
-            while days >= 365 {
+            days_count *= -1;
+            while days_count >= 365 {
                 year -= 1;
-                if is_leapyear(year) {
-                    days -= 366;
-                } else {
-                    days -= 365;
-                }
+                days_count -= find_year_days(year);
             }
             let x: i32 = if is_leapyear(year) { 29 } else { 28 };
             let month_days: [i32; 12] = [31, x, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
             let mut i: i32 = 11;
 
-            while days > month_days[i as usize] {
-                days -= month_days[i as usize];
+            while days_count > month_days[i as usize] {
+                days_count -= month_days[i as usize];
                 i -= 1;
             }
-            month = i +1;
-            day = month_days[i as usize] - days +1
+            month = i + 1;
+            day = month_days[i as usize] - days_count + 1;
         }
         return (year, month, day);
     }
@@ -108,6 +92,14 @@ fn is_leapyear(year: i32) -> bool {
         true
     } else {
         false
+    };
+}
+
+fn find_year_days(year: i32) -> i32 {
+    return if is_leapyear(year) {
+        366
+    } else {
+        365
     };
 }
 
@@ -134,17 +126,14 @@ impl fmt::Display for Date {
 
 fn main() {
     // testing from_ymd; should print Date { days: 738885 }
-
     println!("{:?}", Date::from_ymd(2022, 12, 31));
+
     // testing Add and Display
-
     let date = Date::from_ymd(-2, 12, 31);
-    // increase date by 30 days, 60 days, etc.
 
+    // increase date by 30 days, 60 days, etc.
     for i in 0..20 {
         // first iteration should print 2/12/31 BC
         println!("{}", date + i * 30);
     }
 }
-
-
